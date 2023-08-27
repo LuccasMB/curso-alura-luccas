af3600fae2a0da8a7fadf2533e993a9ed5ae6f14
LuccasMB <luccas.batalha@engenharia.ufjf.br>
2023-08-27 20:21:04 -0300

    Invertendo o lado que a barra de vida diminui

--- unity-parte-4/Assets/Scripts/Billboard.cs
+++ unity-parte-4/Assets/Scripts/Billboard.cs
@@ -12,6 +12,6 @@ public class Billboard : MonoBehaviour
         //Camera.main é o objeto da camera principal
         //nós mandamos o canvas olhar para a posição oposta a frente da camera baseada na posicao que o canvas está por isso tem que subtrair
         //assim onde estiver o canvas sempre irá estar olhando para a camera, de qualquer posicao
-        transform.LookAt(transform.position - Camera.main.transform.forward);  
+        transform.LookAt(transform.position + Camera.main.transform.forward); // normalmente o sinal aqui é de menos, mas como é uma barra de vida, vamos colocar + pois isso vai inverter o lado que a barra de vida diminui
     }
 }
